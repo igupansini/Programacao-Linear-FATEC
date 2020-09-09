e3adc9b2a91e5905baf715756f83ba2712ad684a
Igor Pansini <pansini@outlook.com.br>
2020-09-08 23:23:46 -0300

    Ex 5 concluido

--- Ex_5.py
+++ Ex_5.py
@@ -1,9 +1,17 @@
 carrosConsumo = {"Ferrari": 9.0, "BMW": 11.0, "Audi": 13.0, "Mercedes-Benz": 14.0, "Porsche": 12.0}
 menorConsumo = 0.0
+distancia = 1000.0
+litrosCombustivel = 0.0
+valorGasolina = 4.25
 
 for carro in carrosConsumo:
     if carrosConsumo[carro] >= menorConsumo:
         menorConsumo = carrosConsumo[carro]
         carroEconomico = carro
+for carro in carrosConsumo:
+    litrosCombustivel = distancia / carrosConsumo[carro]
+    custoTotal = litrosCombustivel * valorGasolina
+    print(carro, "consome", round(litrosCombustivel, 2), "litros para percorrer 1.000 quilômetros.")
+    print(carro, "gastará um total de R$", round(custoTotal, 2), "\n")
 
 print("O carro mais econômico é o(a)", carroEconomico,"faz", menorConsumo, "kms com um litro de combustível.")
